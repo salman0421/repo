618ddac8fa871361873c589e214a055daa50d8bf
QAP23 <QAP23@QAP23-PC>
2019-01-22 11:08:41 +0530

    hi

---
+++ src/TestNGDataEx/ParamiterizedTest1.java
@@ -0,0 +1,14 @@
+package TestNGDataEx;
+/*import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class ParameterizedTest1
+{
+	   @Test
+	   //@Parameters("strUN")
+	   public void parameterTest(String strUN)
+	   {
+	      System.out.println("Parameterized value is : " + strUN);
+	   }
+}*/
+
---
+++ src/org/testng/annotations/Test.java
@@ -0,0 +1,9 @@
+package org.testng.annotations;
+/*
+public class Test {
+ @test
+ @Parameters
+ public void parametertest(String StrUN)
+ 
+}
+*/
